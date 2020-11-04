c8b917c0cf9961b879e35a0d37e4d1507975187e
SHAMBASHIB MAJUMDAR <62923672+shambashib20@users.noreply.github.com>
2020-11-04 19:57:21 +0530

    Commiting src folder
    This contains all the essential components of this site!

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+const API_KEY = "8776c1e0b0a2f5ccb2187c420706af96";
+
+// eslint-disable-next-line no-unused-vars
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movies?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`, 
+}
+
+export default requests;
